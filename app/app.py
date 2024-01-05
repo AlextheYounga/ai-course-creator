e3ee9b77169453a64bd607dfceb7e4073ec5ad89
Alex Younger <thealexyounger@proton.me>
2024-01-04 19:27:45 -0500

    feat: dashboard working

--- app/app.py
+++ app/app.py
@@ -1,10 +1,11 @@
 # flask --app app.app run
 from flask import Flask, jsonify, render_template
 from src.utils.compile_course_material import compile_course_material
+from src.utils.files import read_txt_file
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
-app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 
 @app.route("/")
 def home():
@@ -13,7 +14,18 @@ def home():
 
 @app.route('/ajax-fetch-content', methods=['POST'])
 def fetch_content():
+    # Compiling all course material content in a single object
     data = compile_course_material()
+
+    # Returning a response back to the AJAX call
+    return jsonify(data)
+
+
+@app.route('/ajax-fetch-activity', methods=['POST'])
+def fetch_activity():
+    # Reading log file
+    data = open('src/data/chat/logs/chat.log').read().splitlines()
+    data.reverse()
 
     # Returning a response back to the AJAX call
     return jsonify(data)
---
+++ app/static/index.js
@@ -0,0 +1,49 @@
+function translateToTableRows(data) {
+    const rows = []
+    for (topic of data) {
+        if (!topic?.courses?.length) continue
+
+        for (course of topic.courses) {
+            if (!course?.chapters?.length) continue
+
+            for (chapter of course.chapters) {
+                if (!chapter?.pages?.length) continue
+
+                for (page of chapter.pages) {
+                    rows.push({
+                        group: `Course: ${course.courseName}`,
+                        topic: topic.topic,
+                        course: course.courseName,
+                        chapter: chapter.chapterName,
+                        page: page.pageName,
+                        link: page.path,
+                        generated: page.exists
+                    })
+                }
+            }
+        }
+    }
+    return rows
+}
+
+function logColor(line) {
+    let color = '';
+    if (line.includes('RESPONSE')) {
+        return 'text-green-500'
+    }
+    if (line.includes('SEND')) {
+        return 'text-yellow-500'
+    }
+    return color
+}
+
+function displayLogText(line) {
+    if (line.includes('RESPONSE')) {
+        return 'RESPONSE: ' + line
+    }
+    if (line.includes('SEND')) {
+        return 'SEND: ' + line
+    }
+    
+    return line.substring(0, 100)
+}
