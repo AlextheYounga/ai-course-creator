993272483e66c3abe0d939e20d7843e3850307cb
Alex Younger <thealexyounger@proton.me>
2024-01-30 15:34:52 -0500

    feat: add view page functionality

--- app/client/src/views/PageView.vue
+++ app/client/src/views/PageView.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="px-6 py-32 lg:px-8">
-        <div class="mx-auto max-w-3xl text-base leading-7 text-gray-700">
+        <a href="/" class="rounded-md bg-red-800 px-2.5 py-1.5 text-sm font-semibold text-gray-200 shadow-sm hover:bg-white/20">&#8592; Back to Dashboard</a>
+        <div class="mx-auto max-w-3xl text-base leading-7 text-gray-200">
             <div v-if="pageHtml" id="page-body">
                 <div v-html="pageHtml"></div>
             </div>
@@ -14,17 +15,18 @@ import flaskApi from '@/router/api'
 export default {
     name: 'PageView',
     data() {
-        const pageHtml = null;
-        const pageId = this.$route.params.id;
+        const pageHtml: string = "";
+
         return {
-            pageHtml,
-            pageId
+            pageHtml: pageHtml,
         }
     },
 
     async beforeCreate() {
-        const pageContent = await flaskApi.get(`/page/${this.pageId}`)
-        this.pageHtml = pageContent.data.html
+        const pageId = (this.$route.params.id as string)
+        const pageContent = await flaskApi.get(`/page/${pageId}`)
+
+        this.pageHtml = pageContent
     }
 }
 </script>
