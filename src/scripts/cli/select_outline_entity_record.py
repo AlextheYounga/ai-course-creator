a29c05a55bc6acfc67977fe9641f7f1460720b9b
Alex Younger <thealexyounger@proton.me>
2024-03-07 19:19:54 -0500

    fix: correct cli for tree shaking course creation

--- src/scripts/cli/select_outline_entity_record.py
+++ src/scripts/cli/select_outline_entity_record.py
@@ -2,11 +2,11 @@ import inquirer
 from db.db import DB, Topic, Outline, OutlineEntity, Page
 
 
-def _get_outline_entity_from_page(outline: Outline, page: Page):
+def _get_outline_entity_from_item(outline: Outline, entity_type: str, entity_id: int):
     return DB.query(OutlineEntity).filter(
         OutlineEntity.outline_id == outline.id,
-        OutlineEntity.entity_id == page.id,
-        OutlineEntity.entity_type == 'Page'
+        OutlineEntity.entity_id == entity_id,
+        OutlineEntity.entity_type == entity_type
     ).first()
 
 
@@ -35,16 +35,15 @@ def select_outline_entity_record(topic: Topic, hierarchy: str):
     if hierarchy not in ['Course', 'Chapter', 'Page']:
         raise ValueError('Invalid hierarchy level.')
 
-    page_record = None
-
     if hierarchy == 'Course':
-        page_record = _select_record(entities['courses'], 'course')
+        entity_record = _select_record(entities['courses'], 'course')
+        return _get_outline_entity_from_item(outline, hierarchy, entity_record.id)
 
     if hierarchy == 'Chapter':
         chapter_items = [chapter for chapter in entities['chapters'] if chapter.name != 'Final Skill Challenge']
-        page_record = _select_record(chapter_items, 'chapter')
+        entity_record = _select_record(chapter_items, 'chapter')
+        return _get_outline_entity_from_item(outline, hierarchy, entity_record.id)
 
     if hierarchy == 'Page':
-        page_record = _select_record(entities['pages'], 'page')
-
-    return _get_outline_entity_from_page(outline, page_record)
+        entity_record = _select_record(entities['pages'], 'page')
+        return _get_outline_entity_from_item(outline, hierarchy, entity_record.id)
--- src/scripts/initialize.py
+++ src/scripts/initialize.py
@@ -2,8 +2,6 @@ from termcolor import colored
 import os
 import shutil
 
-LOGS_PATH = 'storage/logs'
-
 
 def _create_env_file():
     if not os.path.exists('./.env'):
@@ -14,17 +12,24 @@ def _create_env_file():
     return False
 
 
+def _copy_params_file():
+    if not os.path.exists('params.yaml'):
+        shutil.copy('params.example.yaml', 'params.yaml')
+        return True
+    return False
 
-def _create_log_file():
-    if (not os.path.exists(f"{LOGS_PATH}/app.log")):
-        open(f"{LOGS_PATH}/app.log", 'w').close()
+
+def _copy_topics_file():
+    if not os.path.exists('storage/topics.yaml'):
+        shutil.copy('storage/topics.example.yaml', 'storage/topics.yaml')
         return True
     return False
 
 
 def initialize_project():
     initialized = _create_env_file()
-    initialized = _create_log_file()
+    initialized = _copy_params_file()
+    initialized = _copy_topics_file()
 
     if initialized:
         print(colored("Project initialized.", "green"))
