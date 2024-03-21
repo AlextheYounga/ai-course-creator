933d5804b84f6147acb72f0ba95202a0a49fb476
Alex Younger <thealexyounger@proton.me>
2024-03-20 22:49:27 -0400

    feat: add highlight js and associated styles

--- app/client/src/helpers.js
+++ app/client/src/helpers.js
@@ -1,4 +1,4 @@
-export function capitalize(string: string | null): string | null {
+export function capitalize(string) {
     if (!string) return string;
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
