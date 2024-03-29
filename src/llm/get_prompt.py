47ec557cd4dd637eca7f15a0236456a7b79c3b59
Alex Younger <thealexyounger@proton.me>
2024-03-28 21:45:57 -0400

    feat: default to core prompt path unless collection override exists

--- src/llm/get_prompt.py
+++ src/llm/get_prompt.py
@@ -1,9 +1,16 @@
 from db.db import Topic
+import os
 
 
 def get_prompt(topic: Topic, filename: str, args: dict = {}) -> str:
-    prompt_collection = topic.get_properties().get("prompts", 'default')
-    prompt = open(f"storage/prompts/{prompt_collection}/{filename}.md", "r").read()
+    prompt_collection = topic.get_properties().get("prompts", 'core')
+
+    prompt_path = f"storage/prompts/{prompt_collection}/{filename}.md"
+
+    if not os.path.exists(prompt_path):
+        prompt_path = f"storage/prompts/core/{filename}.md"
+
+    prompt = open(prompt_path, "r").read()
 
     if (args != None):
         for key, value in args.items():
