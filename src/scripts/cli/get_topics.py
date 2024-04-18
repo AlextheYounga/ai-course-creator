44b953e0bce93ea0dc155ab143c9369972f9e919
J.C. Hiatt <jc@jchiatt.com>
2024-04-18 14:04:22 -0500

    feat: can generate drafts for _all_ topics at once

--- src/scripts/cli/get_topics.py
+++ src/scripts/cli/get_topics.py
@@ -1,10 +1,9 @@
 import setproctitle
-import inquirer
 from db.db import DB, Topic
 from src.utils.files import read_yaml_file
 
 
-def select_topic():
+def get_topics():
     topic_records = DB.query(Topic.name).all()
     topic_names_from_db = [record[0] for record in topic_records]
 
@@ -13,17 +12,7 @@ def select_topic():
     topic_names_from_file = list(topics['topics'].keys())
 
     topic_choices = list(set(topic_names_from_db + topic_names_from_file))
-    topic_choices.insert(0, 'All')
 
-    choices = [
-        inquirer.List('topic',
-                      message="Which topic would you like to generate course material for?",
-                      choices=topic_choices),
-    ]
+    setproctitle.setproctitle(topic_choices[0])
 
-    choice = inquirer.prompt(choices)
-    answer = choice['topic']
-
-    setproctitle.setproctitle(answer)
-
-    return answer
+    return topic_choices
